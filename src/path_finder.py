8b235e015de621d29b6185468e780f4bef0c44dd
pablomalvido <pablo.malvidofresnillo@tuni.fi>
2024-01-26 17:34:30 +0200

    path_finder2

--- src/path_finder.py
+++ src/path_finder.py
@@ -76,6 +76,13 @@ def check_collisions(p1, end, R=R_collision):
     return False
     
 
+def check_repeated(p, p_list, eps = step_increase/10):
+    for p_list_i in p_list:
+        if dist_2d(p, p_list_i)<eps:
+            return True #It is repeated
+    return False
+
+
 def interpolate_next(p1,p2,step=0.001):
     dist = dist_2d(p1,p2)
     p3 = [p1[0]+(p2[0]-p1[0]) * step / dist, p1[1]+(p2[1]-p1[1]) * step / dist]
@@ -83,11 +90,13 @@ def interpolate_next(p1,p2,step=0.001):
 
 
 def get_path(p, end, length = 0, prev = 0):
+    #print("GET")
     i=0
     straight = True
-    while dist_2d(p[-1], end) > max(R_collision + step_increase, 0.025):
+    while dist_2d(p[-1], end) > max(R_collision + 2*step_increase, 0.02):
         p_test = interpolate_next(p[-1], end, step=step_increase)
         if not check_collisions(p_test, end):
+            #print("STRAIGHT")
             p.append(p_test)
             length += step_increase
             prev = 0
@@ -106,14 +115,19 @@ def get_path(p, end, length = 0, prev = 0):
                 p_new = copy.copy(p_copy[-1])
                 p_new[1] += lateral_increase
                 if not check_collisions(p_new, end):
-                    p_copy.append(p_new)
+                    if check_repeated(p_new, p_copy):
+                        p1 = p_copy
+                        length1 = length_copy
+                        break
+                    else:
+                        p_copy.append(p_new)
                 else:
                     # print('error')
                     success1 = False
                 if success1:
                     # print("P1")
                     # print(p_copy[-1])
-                    # rospy.sleep(1)
+                    # rospy.sleep(0.2)
                     p1, length1, success1 = get_path(p_copy,end_copy,length_copy,prev=1)
 
             if prev != 1:
@@ -123,14 +137,19 @@ def get_path(p, end, length = 0, prev = 0):
                 p_new2 = copy.copy(p_copy2[-1])
                 p_new2[1] -= lateral_increase
                 if not check_collisions(p_new2, end):
-                    p_copy2.append(p_new2)
+                    if check_repeated(p_new2, p_copy2):
+                        p2 = p_copy2
+                        length2 = length_copy2
+                        break
+                    else:
+                        p_copy2.append(p_new2)
                 else:
                     # print('error')
                     success2 = False
                 if success2:
                     # print("P2")
                     # print(p_copy2[-1])
-                    # rospy.sleep(1)
+                    # rospy.sleep(0.2)
                     p2, length2, success2 = get_path(p_copy2,end_copy2,length_copy2,prev=2)
 
             if (not success1 or prev==2) and (not success2 or prev==1):
@@ -143,7 +162,7 @@ def get_path(p, end, length = 0, prev = 0):
             return [], 1000000, False
 
     if not straight:
-        if length1 > length2:
+        if length1 >= length2:
             p = p2
             length = length2
         else:
@@ -153,14 +172,32 @@ def get_path(p, end, length = 0, prev = 0):
     return p, length, True
 
 
-def plot_path(p, end, angle, offset):
-    p_con = [cable_to_EE(p[0], angle, offset)]
-    end_con = cable_to_EE(end, angle, offset)
+def plot_path(p, end, end_pose, angle, offset_cable, offset_z, interpolate_z):
+    p_con = [cable_to_EE(p[0], angle, offset_cable)]
+    end_con = cable_to_EE(end, angle, offset_cable)
     p_con, length, success = get_path(p_con, end_con)
+    if not success:
+        return [], False
+    length += dist_2d(p_con[-1], end_con)
     p_con.append(end_con)
     p_EE = []
+    p_pose_EE = []
+    length_accum = 0
     for p_con_i in p_con:
-        p_EE.append(EE_to_cable(p_con_i, angle, offset))
+        p_EE.append(EE_to_cable(p_con_i, angle, offset_cable))
+        new_pose = Pose()
+        new_pose.position.x = p_EE[-1][0]
+        new_pose.position.y = p_EE[-1][1]
+        if interpolate_z:
+            if len(p_EE)>1:
+                length_accum += dist_2d(p_EE[-2], p_EE[-1])
+            new_pose.position.z = end_pose.position.z + (offset_z * (1-(length_accum/length)))
+        else:
+            new_pose.position.z = end_pose.position.z + offset_z 
+        new_pose.orientation = copy.copy(end_pose.orientation)
+        p_pose_EE.append(new_pose)
+    p_EE[-1] = end
+    p_pose_EE[-1] = end_pose
 
     x_path = []
     y_path = []
@@ -179,7 +216,7 @@ def plot_path(p, end, angle, offset):
     xTraj1 = np.array(x_path1)
     yTraj1 = np.array(y_path1)
     plt.plot(xTraj1, yTraj1, color='pink')
-    return p_EE
+    return p_pose_EE, True
 
 
 def find_path_cb(req):
@@ -207,24 +244,31 @@ def find_path_cb(req):
     end_guide = req.end_guide
     plot_EP_x = []
     plot_EP_y = []
-    for guide in guides:
-        if guide.id == end_guide:
-            end_point = [guide.key_center_frame.position.x, guide.key_center_frame.position.y]
-            end_pose = guide.key_center_frame
-            plot_EP_x.append(guide.key_center_frame.position.x)
-            plot_EP_y.append(guide.key_center_frame.position.y)
+    if req.target_provided:
+        end_point = [req.target_pose.position.x, req.target_pose.position.y]
+        end_pose = req.target_pose
+    else:
+        for guide in guides:
+            if guide.id == end_guide:
+                end_point = [guide.key_center_frame.position.x, guide.key_center_frame.position.y]
+                end_pose = guide.key_center_frame
+    print(req.target_pose)
+    print(end_point)
+    plot_EP_x.append(end_point[0])
+    plot_EP_y.append(end_point[1])
     xEpoints = np.array(plot_EP_x)
     yEpoints = np.array(plot_EP_y)
     plt.plot(xEpoints, yEpoints, 'o', color='g')
 
     end_frame = pose_to_frame(end_pose)
     rotZ = end_frame.M.GetEulerZYX()[0] * 180/math.pi
-    wp = plot_path([starting_point], end_point, rotZ, length_cable[req.WH]) #Include orientation and Z
+    wp, success = plot_path([starting_point], end_point, end_pose, rotZ, offset_cable = length_cable[req.WH], offset_z = req.offset_z, interpolate_z = req.interpolate_z) #Include orientation and Z
     if req.show:
         plt.show()
     resp = pathFinderResponse()
-    # for pose in wp:
-    #     resp.path.append(wp)
+    for pose in wp:
+        resp.path.append(pose)
+    resp.success = success
     return resp    
 
 rospy.Service("/find_free_path", pathFinder, find_path_cb)
